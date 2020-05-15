ceea9bb2bdc60bf008ee2141d93751247bc91dc4
klopfstein <dimitri.kft@gmail.com>
2020-05-15 15:56:05 +0200

    add entity fammill for relationship with animal and add fixture famille

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -3,59 +3,80 @@
 namespace App\DataFixtures;
 
 use App\Entity\Animal;
+use App\Entity\Famille;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AnimalFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        $a1 = new Animal();
-        $a1->setNom("Chien")
-           ->setDescription("Un animal domestique")
-           ->setImage("chien.png")
-           ->setPoids(10)
-           ->setDangereux(false)
-        ;
-        $manager->persist($a1);
+   public function load(ObjectManager $manager)
+   {
+      $c1 = new Famille();
+      $c1->setLibelle("Mammifères")
+         ->setDescription("Animaux vertébrés nourissant leurs petits avec du lait");
+      $manager->persist($c1);
 
-        $a2 = new Animal();
-        $a2->setNom("Cochon")
-           ->setDescription("Un animal d'élevage")
-           ->setImage("cochon.png")
-           ->setPoids(300)
-           ->setDangereux(false)
-        ;
-        $manager->persist($a2);
+      $c2 = new Famille();
+      $c2->setLibelle("Reptiles")
+         ->setDescription("Animaux vertébrés qui rampent");
+      $manager->persist($c2);
 
-        $a3 = new Animal();
-        $a3->setNom("Serpent")
-           ->setDescription("Un animal dangereux")
-           ->setImage("Serpent.png")
-           ->setPoids(5)
-           ->setDangereux(true)
-        ;
-        $manager->persist($a3);
+      $c3 = new Famille();
+      $c3->setLibelle("Poissons")
+         ->setDescription("Animaux vertébrés invertébrés du monde aquatique");
+      $manager->persist($c3);
 
-        $a4 = new Animal();
-        $a4->setNom("Crocodile")
-           ->setDescription("Un animal très dangereux")
-           ->setImage("croco.png")
-           ->setPoids(500)
-           ->setDangereux(true)
-        ;
-        $manager->persist($a4);
+      $a1 = new Animal();
+      $a1->setNom("Chien")
+        ->setDescription("Un animal domestique")
+        ->setImage("chien.png")
+        ->setPoids(10)
+        ->setDangereux(false)
+        ->setFamille($c1)
+      ;
+      $manager->persist($a1);
 
-        $a5 = new Animal();
-        $a5->setNom("Requin")
-           ->setDescription("Un animal marin très dangereux")
-           ->setImage("requin.png")
-           ->setPoids(800)
-           ->setDangereux(true)
-        ;
-        $manager->persist($a5);
+      $a2 = new Animal();
+      $a2->setNom("Cochon")
+         ->setDescription("Un animal d'élevage")
+         ->setImage("cochon.png")
+         ->setPoids(300)
+         ->setDangereux(false)
+         ->setFamille($c1)
+      ;
+      $manager->persist($a2);
+
+      $a3 = new Animal();
+      $a3->setNom("Serpent")
+         ->setDescription("Un animal dangereux")
+         ->setImage("Serpent.png")
+         ->setPoids(5)
+         ->setDangereux(true)
+         ->setFamille($c2)
+      ;
+      $manager->persist($a3);
+
+      $a4 = new Animal();
+      $a4->setNom("Crocodile")
+         ->setDescription("Un animal très dangereux")
+         ->setImage("croco.png")
+         ->setPoids(500)
+         ->setDangereux(true)
+         ->setFamille($c2)
+      ;
+      $manager->persist($a4);
+
+      $a5 = new Animal();
+      $a5->setNom("Requin")
+         ->setDescription("Un animal marin très dangereux")
+         ->setImage("requin.png")
+         ->setPoids(800)
+         ->setDangereux(true)
+         ->setFamille($c3)
+      ;
+      $manager->persist($a5);
     
 
-        $manager->flush();
+      $manager->flush();
     }
 }
